d3ade2897116fefbd17ebe2007813d2401da9bd4
root <root@DESKTOP-OPTI8OD>
2025-01-03 14:20:55 +0800

    init project

---
+++ guli-product/src/test/java/com/atguigu/guliproduct/GuliProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guliproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
